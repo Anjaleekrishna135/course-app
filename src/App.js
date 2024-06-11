b36279e643d15e697839f9bc9c00f120649dbf65
Anjaleekrishna135 <anjaleekrishna135@gmail.com>
2024-06-11 21:27:33 +0530

    changes commited

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import AddCourse from './components/AddCourse';
 import Search from './components/Search';
 import View from './components/View';
 import { NavBar } from './components/NavBar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddCourse />
-      <Search />
-      <View />
-      <NavBar />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AddCourse />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/view' element={<View />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const NavBar = () => {
     return (
         <div>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">course</a>
+                    <Link class="navbar-brand" to="#">course</Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="AddCourse.jsx">add</a>
+                                <Link class="nav-link active" aria-current="page" to="/">add</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="Search.jsx">search</a>
+                                <Link class="nav-link" to="/search">search</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="View.jsx">view</a>
+                                <Link class="nav-link" to="/view">view</Link>
                             </li>
 
 
