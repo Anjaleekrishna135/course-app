cf030e18185871a391a4e98d668d9188a6ad61ea
Anjaleekrishna135 <anjaleekrishna135@gmail.com>
2024-06-11 20:40:49 +0530

    project created

---
+++ src/components/AddCourse.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const AddCourse = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">course title</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">course description</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">course date</label>
+                                <input type="date" name="" id="" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">duration</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">venue</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">trainer name</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <center>
+                                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                    <button className="btn btn-success">submit</button>
+                                </div>
+                            </center>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AddCourse
---
+++ src/components/View.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const View = () => {
+    return (
+        <div>
+            <table class="table">
+                <thead>
+                    <tr>
+
+                        <th scope="col">course title</th>
+                        <th scope="col">course description</th>
+                        <th scope="col">date</th>
+                        <th scope="col">duration</th>
+                        <th scope="col">venue</th>
+                        <th scope="col">trainer name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+
+                        <td>SE</td>
+                        <td>software engineering</td>
+                        <td>2-4-24</td>
+                        <td>5</td>
+                        <td>Fisat</td>
+                        <td>Rakhi</td>
+                    </tr>
+                    <tr>
+
+                        <td>stati</td>
+                        <td>applied statitics</td>
+                        <td>4-4-24</td>
+                        <td>6</td>
+                        <td>fisat</td>
+                        <td>rose</td>
+                    </tr>
+                    <tr>
+                        <td>oops</td>
+                        <td>java</td>
+                        <td>6-4-24</td>
+                        <td>6</td>
+                        <td>fisat</td>
+                        <td>manju</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default View
